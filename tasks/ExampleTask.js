5b1afd7af0ee750fcd53ed40ef038cbbeac09ce9
Richard Gustin <richard.gustin86@gmail.com>
2014-06-26 04:30:32 +1000

    feat(task): Added an example task

---
+++ tasks/ExampleTask.js
@@ -0,0 +1,25 @@
+var Task = require( 'classes' ).task
+  , async = require( 'async' )
+  , debug = require( 'debug' )( 'ExampleTask' );
+ 
+module.exports = Task.extend(
+{
+    init: function( payload, callback ) {
+        async.parallel([
+            this.proxy( 'hello' ),
+            this.proxy( 'name' )
+        ],
+        function(err, results){
+            debug( 'Finished.' );
+            callback( err, results.join( ' ' ) );
+        });
+    },
+ 
+    hello: function( callback ) {
+        callback( null, 'Hello' );
+    },
+
+    name: function( callback ) {
+        callback( null, this.payload.name );
+    }
+});
